eabfb93efd22ecbc2899128e5d58b8afd43f486f
Yurii <nais30055003@gmail.com>
2021-11-13 14:05:27 +0300

    Initial commit

---
+++ PZ_5/PZ_5(2).py
@@ -0,0 +1,13 @@
+# функция которая возвращает количество C целых чисел числа K и их сумму S
+def digitcountsum(k):
+    c = 0
+    s = 0
+    for i in k:
+        c += 1
+        s += int(i)
+    return c, s
+
+
+# пример работы функции:
+for n in range(5):
+    print('Количество цифр: {}. Сумма цифр: {}'.format(*digitcountsum(input('Введите число: '))))
