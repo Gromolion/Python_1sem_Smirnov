ab010baae7ea8e1dcd24d884bfdfd08e3629ac41
Storn25 <nikita.novichev@yandex.ru>
2022-01-22 00:11:22 +0300

    init

--- PZ_12/PZ_12(1).py
+++ PZ_12/PZ_12(1).py
@@ -1,11 +1,16 @@
-from tkinter import *
+# В соответствии с номером варианта перейти по ссылке на прототип. Реализовать
+# его в IDE PyCharm Community с применением пакета tk. Получить интерфейс
+# максимально приближенный к оригиналу (см. таблицу 1).
 
+from tkinter import *
 
 root = Tk()
 root.title('Форма заявки')
 root.geometry('550x470+500+100')
+root.resizable(0, 0)
+root.configure(background='white')
 
-
+# создаю экземпляры объектов рамок, текста, кнопок и текстовых полей
 frame1 = Frame(highlightthickness=1,
                highlightbackground="#64D8D1",
                width=506,
@@ -161,6 +166,7 @@ frame16 = Frame(highlightthickness=1,
 button16_1 = Button(text='Отправить Email', font='Aerial 10')
 button16_2 = Button(text='Очистить', font='Aerial 10')
 
+# размещаю рамки в интерфейсе
 frame1.place(x=20, y=16)
 frame2.place(x=20, y=39)
 frame3.place(x=20, y=102)
@@ -178,6 +184,7 @@ frame14.place(x=172, y=237)
 frame15.place(x=20, y=263)
 frame16.place(x=20, y=416)
 
+# размещаю текст в интерфейсе
 label1.place(x=21, y=17, width=504, height=22)
 label2_1.place(x=21, y=45, width=180, height=18)
 label2_2.place(x=200, y=45)
@@ -196,6 +203,7 @@ label10.place(x=383, y=135, height=25)
 label15_1.place(x=25, y=265)
 label15_2.place(x=158, y=269, height=25)
 
+# размещаю поля ввода текста
 entry9.place(x=177, y=106, width=205)
 entry10.place(x=177, y=135, width=205)
 entry11.place(x=177, y=160, width=205)
@@ -204,10 +212,12 @@ entry13.place(x=177, y=214, width=205)
 entry14.place(x=177, y=241, width=205)
 text15.place(x=56, y=288, width=436, height=120)
 
+# размещаю кнопки
 button12.place(x=385, y=187, height=21)
 button13.place(x=385, y=214, height=21)
 button14.place(x=385, y=241, height=21)
 button16_1.place(x=160, y=421, height=21, width=120)
 button16_2.place(x=290, y=421, height=21, width=80)
 
+# запускаю цикл работы программы
 root.mainloop()
---
+++ PZ_12/PZ_12(2).py
@@ -0,0 +1,48 @@
+# разработать программу с применением пакета tk, взяв в качестве условия
+# одну любую задачу из ПЗ №3-8.
+
+from tkinter import *
+from tkinter import messagebox
+
+
+def show_msg():
+    # функция отображения результата
+    try:
+        x = int(inp_x.get())
+        y = int(inp_y.get())
+
+    except ValueError:
+        messagebox.showerror("Ошибка", 'Одно из введенных значений не является числом')
+
+    else:
+        result = 'лежит' if x < 0 < y else 'не лежит'
+        messagebox.showinfo('Вывод', f'Точка с заданными координатами {result} во второй четверти')
+
+
+root = Tk()
+root.title('ПЗ_3_1')
+root.geometry('600x300+400+100')
+root['bg'] = '#B0E0E6'
+
+
+cond = Label(text='Программа для проверки, лежит ли точка\n с заданными координатами во второй четверти',
+             font='Aerial 14 bold', bg='#B0E0E6', pady=15)
+cond.pack()
+
+frame = Frame(pady=10, padx=10, bg='#B0E0E6')
+frame.pack(fill='x')
+txt_x = Label(frame, text='Введите x:', font='Aerial 12', bg='#B0E0E6')
+inp_x = Entry(frame, bg='lightgreen')
+txt_x.pack(side='left')
+inp_x.pack(side='left')
+
+txt_y = Label(frame, text='Введите y:', font='Aerial 12', bg='#B0E0E6')
+inp_y = Entry(frame, bg='lightgreen')
+inp_y.pack(side='right')
+txt_y.pack(side='right')
+
+# кнопка, которая при нажатия вызывает функцию show_msg
+button = Button(text='Получить ответ', bg='lightgreen', command=show_msg)
+button.pack()
+
+root.mainloop()
